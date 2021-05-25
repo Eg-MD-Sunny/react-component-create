48265b6b6b1f4a32e79413373c75e1c967f4b3b6
Naim Zaman <egnaimur@gmail.com>
2021-05-25 12:47:53 +0600

    Similar In look Similar In Data

--- src/App.js
+++ src/App.js
@@ -5,21 +5,39 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <p>I am a Personal React</p>
+
+          <Person></Person>   {/*<<============Component Call*/}
+          <Person></Person>   {/*<<============Component Call*/}
+          <Person></Person>   {/*<<============Component Call*/}
+          <Person></Person>   {/*<<============Component Call*/}
       </header>
     </div>
   );
+}
+
+//============>>Create Component
+function Person(){
+
+    {/*Component Style Start*/} //<<======2nd Way
+      const personStyle={
+        border:'2px solid yellow',
+        marginBottom:'10px',
+        borderRadius:'7px',
+        padding:'10px',
+        boxSizing:'border-box'
+      };
+    {/*Component Style End*/}
+
+    return (
+      // <div style={{border:'2px solid yellow',borderRadius:'7px',marginBottom:'10px'}}>  //<<======1st Way
+      <div style={personStyle}>
+        <div>
+            <h1>Name: Sakib-Al-Hasen</h1>
+            <h3>Hero of the year</h3>
+        </div>
+      </div>  
+    )
 }
 
 export default App;
